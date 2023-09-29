ba55afdcff0327646bacc50329739446ef0ebaae
francisco <francisco.infanti@gmail.com>
2023-09-29 12:12:26 -0300

    Implemento funcion de crear, insertar, buscar, eliminar y hago algunas pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,104 @@
 #include "pa2m.h"
+#include "src/abb.h"
 
-void prueba_simple()
+int comparador(void *e1, void *e2)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	int *numero = e1;
+	int *otro_numero = e2;
+	if (*numero == *otro_numero)
+		return 0;
+	else if (*numero > *otro_numero)
+		return 1;
+	else
+		return -1;
+}
+
+void destructor(void *elemento)
+{
+	int *numero = elemento;
+	printf(" (%i) ", *numero);
+}
+
+void se_puede_crear_un_arbol()
+{
+	abb_t *arbol = abb_crear(comparador);
+	pa2m_afirmar(arbol != NULL, "Puedo crear un arbol");
+	abb_destruir(arbol);
+}
+
+void no_se_puede_crear_un_arbol_con_un_comparador_nulo()
+{
+	abb_t *arbol = abb_crear(NULL);
+	pa2m_afirmar(arbol == NULL,
+		     "No puede crearse un arbol con un comparador nulo");
+}
+
+void un_arbol_recien_creado_tiene_cero_elementos()
+{
+	abb_t *arbol = abb_crear(comparador);
+	pa2m_afirmar(abb_tamanio(arbol) == 0,
+		     "Un arbol recien creado tiene cero elementos");
+	abb_destruir(arbol);
+}
+
+void no_se_puede_insertar_elementos_en_un_arbol_nulo()
+{
+	abb_t *arbol = NULL;
+	pa2m_afirmar(abb_insertar(arbol, NULL) == NULL,
+		     "No se puede insertar elementos en una lista null");
+}
+
+void podemos_insertar_un_elemento()
+{
+	abb_t *arbol = abb_crear(comparador);
+	int numero = 15;
+	arbol = abb_insertar(arbol, &numero);
+	pa2m_afirmar(abb_buscar(arbol, &numero) == &numero,
+		     "El elemento insertado es correcto");
+	pa2m_afirmar(abb_tamanio(arbol) == 1,
+		     "La cantidad de elementos agregada es correcta");
+	abb_destruir(arbol);
+}
+
+void podemos_insertar_varios_elementos()
+{
+	abb_t *arbol = abb_crear(comparador);
+	int numeros[] = { 15, 21, 12, 15, 24, 7, 14, 29, 5, 20, 23, 9, 11 };
+	for (int i = 0; i < sizeof(numeros) / sizeof(int); i++) {
+		arbol = abb_insertar(arbol, &numeros[i]);
+		pa2m_afirmar(*(int *)abb_buscar(arbol, &numeros[i]) ==
+				     numeros[i],
+			     "El elemento insertado es correcto");
+	}
+	pa2m_afirmar(abb_tamanio(arbol) == sizeof(numeros) / sizeof(int),
+		     "La cantidad de elementos agregada es correcta");
+	abb_destruir_todo(arbol, destructor);
+}
+
+void prueba_destructor()
+{
+	abb_t *arbol = abb_crear(comparador);
+	int numeros[] = { 15, 21, 12, 15, 23, 7, 14 };
+	for (int i = 0; i < sizeof(numeros) / sizeof(int); i++) {
+		arbol = abb_insertar(arbol, &numeros[i]);
+	}
+	abb_destruir_todo(arbol, destructor);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	pa2m_nuevo_grupo("Pruebas de crear un arbol");
+	se_puede_crear_un_arbol();
+	no_se_puede_crear_un_arbol_con_un_comparador_nulo();
+	un_arbol_recien_creado_tiene_cero_elementos();
+
+	pa2m_nuevo_grupo("Pruebas insercion elementos");
+	no_se_puede_insertar_elementos_en_un_arbol_nulo();
+	podemos_insertar_un_elemento();
+	podemos_insertar_varios_elementos();
+
+	pa2m_nuevo_grupo("Pruebas de destructor");
+	prueba_destructor();
 
 	return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -3,13 +3,103 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/*
+ * Crea un nodo con el elemento pasado por parametro.
+ */
+struct nodo_abb *crear_nodo(void *elemento)
+{
+	struct nodo_abb *nodo = calloc(1, sizeof(struct nodo_abb));
+	if (nodo == NULL)
+		return NULL;
+	nodo->elemento = elemento;
+	return nodo;
+}
+
+/*
+ * Recorre el arbol hasta encontrar la posicion donde se puede insertar el nuevo elemento.
+ * Una vez la encuentre devuelve un puntero a su padre.
+ */
+struct nodo_abb *buscar_posicion(void *nuevo_elemento, struct nodo_abb *actual,
+				 struct nodo_abb *anterior,
+				 abb_comparador comparador)
+{
+	if (actual == NULL)
+		return anterior;
+
+	if (comparador(actual->elemento, nuevo_elemento) > 0)
+		return buscar_posicion(nuevo_elemento, actual->izquierda,
+				       actual, comparador);
+
+	else if (comparador(actual->elemento, nuevo_elemento) < 0)
+		return buscar_posicion(nuevo_elemento, actual->derecha, actual,
+				       comparador);
+
+	else
+		return buscar_posicion(nuevo_elemento, actual->izquierda,
+				       actual, comparador);
+}
+
+/*
+ * Busca el elemento pasado por parametro en el arbol, si lo encuentra lo devuelve.
+ * Si no lo encuentra devuelve NULL.
+ */
+void *buscar_elemento(void *elemento, struct nodo_abb *actual,
+		      abb_comparador comparador)
+{
+	if (actual == NULL)
+		return NULL;
+	if (comparador(elemento, actual->elemento) == 0)
+		return actual->elemento;
+	else if (comparador(elemento, actual->elemento) > 0)
+		return buscar_elemento(elemento, actual->derecha, comparador);
+	else
+		return buscar_elemento(elemento, actual->izquierda, comparador);
+}
+
+/*
+ * Recorre el arbol usando el recorrido Postorden y libera la memoria ocupada por cada nodo. 
+ */
+void eliminar_nodos(struct nodo_abb *actual, void (*destructor)(void *))
+{
+	if (actual->izquierda != NULL)
+		eliminar_nodos(actual->izquierda, destructor);
+	if (actual->derecha != NULL)
+		eliminar_nodos(actual->derecha, destructor);
+	if (destructor != NULL)
+		destructor(actual->elemento);
+	free(actual);
+	return;
+}
+
 abb_t *abb_crear(abb_comparador comparador)
 {
-	return NULL;
+	if (comparador == NULL)
+		return NULL;
+	struct abb *arbol = calloc(1, sizeof(struct abb));
+	if (arbol == NULL)
+		return NULL;
+	arbol->comparador = comparador;
+	return arbol;
 }
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
+	if (arbol == NULL)
+		return NULL;
+
+	struct nodo_abb *nuevo = crear_nodo(elemento);
+	if (nuevo == NULL)
+		return NULL;
+
+	struct nodo_abb *ultimo = buscar_posicion(elemento, arbol->nodo_raiz,
+						  NULL, arbol->comparador);
+	if (ultimo == NULL)
+		arbol->nodo_raiz = nuevo;
+	else if (arbol->comparador(ultimo->elemento, nuevo->elemento) > 0)
+		ultimo->izquierda = nuevo;
+	else
+		ultimo->derecha = nuevo;
+	arbol->tamanio++;
 	return arbol;
 }
 
@@ -20,25 +110,42 @@ void *abb_quitar(abb_t *arbol, void *elemento)
 
 void *abb_buscar(abb_t *arbol, void *elemento)
 {
-	return elemento;
+	if (arbol == NULL)
+		return NULL;
+	if (arbol->tamanio == 0)
+		return NULL;
+
+	return buscar_elemento(elemento, arbol->nodo_raiz, arbol->comparador);
 }
 
 bool abb_vacio(abb_t *arbol)
 {
-	return true;
+	if (arbol == NULL)
+		return true;
+	return arbol->tamanio == 0;
 }
 
 size_t abb_tamanio(abb_t *arbol)
 {
-	return 0;
+	return arbol != NULL ? arbol->tamanio : 0;
 }
 
 void abb_destruir(abb_t *arbol)
 {
+	if (arbol == NULL)
+		return;
+	if (arbol->tamanio != 0)
+		eliminar_nodos(arbol->nodo_raiz, NULL);
+	free(arbol);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
+	if (arbol == NULL)
+		return;
+	if (arbol->tamanio != 0)
+		eliminar_nodos(arbol->nodo_raiz, destructor);
+	free(arbol);
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
